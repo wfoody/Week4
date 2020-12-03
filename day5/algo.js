b1056f055fb7b2fe68641f9e5be2f984dc27a56b
wfoody <foodyw@gmail.com>
2020-12-03 18:02:38 -0500

    rewriting history

---
+++ day5/algo.js
@@ -0,0 +1,27 @@
+// Adding up from a single number
+
+num = 600
+
+let result = (num*(num+1))/2
+
+
+console.log(result)
+
+
+// Which number is larger?
+
+
+
+whichNumber(f,g); {
+    if(f < g) {
+        console.log("g")
+    }else if( f > g) {
+        console.log("f")
+    }else if( f == g) {
+        console.log("neither")
+    }
+}
+
+let number = whichNumber(3,4)
+
+console.log(number)
